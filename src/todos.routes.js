d27e69a69854d022c032af94ec59868ac04b09f8
jcleston <janes.cleston.silva@gmail.com>
2022-04-20 22:17:10 -0300

    Atualização

--- src/todos.routes.js
+++ src/todos.routes.js
@@ -9,6 +9,7 @@ const todosRoutes = express.Router()
 
 //Importar o Prisma Client
 const { PrismaClient } = require("@prisma/client")
+const req = require("express/lib/request")
 
 //Criando uma instância do Prisma Client
 const prisma = new PrismaClient()
@@ -30,7 +31,32 @@ todosRoutes.get("/todos", async (req, res) => {
     return res.status(200).json(todos)
 })
 
-// U
+// Update
+todosRoutes.put("/todos", async (req, res) => {
+    const { name, id, status } = req.body
+
+    if (!id) {
+        return res.status(400).json("Id obrigatório")
+    }
+
+    const todoAlreadyExist = await prisma.todo.findUnique({ where: { id } })
+
+    if (!todoAlreadyExist) {
+        return res.status(404).json("Id não encontrado")
+    }
+
+    const todo = await prisma.todo.update({
+        where: {
+            id,
+        },
+        data: {
+            name,
+            status,
+        },
+    })
+    return res.status(200).json(todo)
+})
+
 // D
 
 //Expotar rotas
